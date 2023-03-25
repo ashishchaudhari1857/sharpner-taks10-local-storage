ec1550d30ecd0d8cb972589a06d391edae5c6e41
Ashish Chaudhari <122347602+ashishchaudhari1857@users.noreply.github.com>
2023-03-25 13:15:40 +0530

    task14

--- booking app/my.js
+++ booking app/my.js
@@ -20,17 +20,31 @@ localStorage.setItem(obj.name,JSON.stringify(obj));
 function showonscreen(obj){
    var parent=document.getElementById('demo');
    var btn =document.createElement('button');
+   var ebtn=document.createElement('button');
+   
    btn.appendChild(document.createTextNode("delete"));
+   ebtn.appendChild(document.createTextNode("edit"));
    var  li =document.createElement('li');
    var  text=document.createTextNode(`${obj.name} ${obj.contact} ${obj.date}`);
    li.appendChild(text);
    li.appendChild(btn);
+   li.appendChild(ebtn);
    parent.appendChild(li);
    btn.addEventListener('click' ,del);
+   ebtn.addEventListener('click' ,edit);
    function  del(e){
       localStorage.removeItem(obj.name);
       parent.removeChild(li);
-       
    }
+   function edit(e){
+      localStorage.removeItem(obj.name);
+      parent.removeChild(li);
+      document.getElementById('name').value=obj.name
+     document.getElementById('contact').value=obj.contact;
+     document.getElementById('date').value=obj.date;
+   }
+
+   
+}
    
 }
